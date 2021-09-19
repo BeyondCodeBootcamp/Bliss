4f37f854a0c7ef5866fd6880051112bd4e8bdc5f
AJ ONeal <aj@therootcompany.com>
2021-09-19 07:55:47 +0000

    feature: use genenv for client-side ENVs

---
+++ env.tpl.js
@@ -0,0 +1,12 @@
+var ENV = {};
+(function () {
+  "use strict";
+
+  //ENV.GOOGLE_CLIENT_ID = "${GOOGLE_CLIENT_ID}";
+  //ENV.GOOGLE_REDIRECT_URI;
+
+  ENV.BASE_API_URL;
+
+  ENV.GITHUB_CLIENT_ID;
+  ENV.GITHUB_REDIRECT_URI;
+})();
---
+++ genenv.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let args = process.argv.slice(2);
+if (!args.length) {
+  require("dotenv").config();
+} else {
+  args.forEach(function (path) {
+    require("dotenv").config({ path: path });
+  });
+}
+
+let fs = require("fs");
+let path = require("path");
+let envjs = fs.readFileSync(path.join(__dirname, "env.tpl.js"), "utf8");
+
+console.info("");
+Object.keys(process.env).forEach(function (envname) {
+  // match, literally "${GOOGLE_CLIENT_ID}" (including quotes)
+  let reVar = new RegExp('"\\$\\{' + envname + '\\}"', "g");
+  let newEnvjs = envjs.replace(reVar, JSON.stringify(process.env[envname]));
+
+  // match ENV.GOOGLE_CLIENT_ID;
+  // becomes ENV.GOOGLE_CLIENT_ID = "xxxx";
+  let reExp = new RegExp("ENV\\." + envname + ";", "g");
+  newEnvjs = newEnvjs.replace(
+    reExp,
+    "ENV." + envname + " = " + JSON.stringify(process.env[envname]) + ";"
+  );
+
+  if (envjs !== newEnvjs) {
+    console.info("Adding", envname, "...");
+  }
+  envjs = newEnvjs;
+});
+
+//let outfile = "./public/js/env.js";
+let outfile = "./env.js";
+fs.writeFileSync(outfile, envjs, "utf8");
+console.info("Wrote", outfile, "from .env (and other existing ENVs)");
+console.info("");
