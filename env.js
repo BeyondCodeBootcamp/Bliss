2abb32f5ed8bbb058fb3f17645bfd86e009443ad
AJ ONeal <aj@therootcompany.com>
2021-10-02 08:34:45 +0000

    deploy: add env.js for gh pages

---
+++ env.js
@@ -0,0 +1,12 @@
+var ENV = {};
+(function () {
+  "use strict";
+
+  //ENV.GOOGLE_CLIENT_ID = "${GOOGLE_CLIENT_ID}";
+  //ENV.GOOGLE_REDIRECT_URI;
+
+  ENV.BASE_API_URL = "https://bliss-api.therootcompany.com";
+
+  ENV.GITHUB_CLIENT_ID = "a2f987e542039c6d8492";
+  ENV.GITHUB_REDIRECT_URI = "https://bliss-api.therootcompany.com/api/authn/webhooks/oauth2/github.com";
+})();
